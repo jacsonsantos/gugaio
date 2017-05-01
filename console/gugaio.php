22ee4e812ed24e087b79b133a7230fabe530c887
Jacson Santos <jacsos-@hotmail.com>
2017-05-01 18:27:12 -0300

    adicionado comando novo e atualizacao em doc

--- console/gugaio.php
+++ console/gugaio.php
@@ -8,6 +8,7 @@ use JSantos\Command\CreateControllerCommand;
 use JSantos\Command\ViewsCommand;
 use JSantos\Command\RegisterImapCommand;
 use JSantos\Command\RegisterMailCommand;
+use JSantos\Command\CreateRepositoryCommand;
 
 $console = new Application();
 //NEW COMMAND
@@ -17,6 +18,7 @@ $console->add(new CreateControllerCommand());
 $console->add(new ViewsCommand());
 $console->add(new RegisterImapCommand());
 $console->add(new RegisterMailCommand());
+$console->add(new CreateRepositoryCommand());
 
 //The End Command
 $console->run();
